936a72e7d137286d6af6ba424b15b96de7761348
Diego Bueno <diego@gwaya.com>
2021-02-05 20:23:55 +1000

    Submitted version
    Submitted version

--- simulating.py
+++ simulating.py
@@ -10,9 +10,6 @@ e-mail: d.bueno.da.silva.10@student.scu.edu.au / i.sypott.10@student.scu.edu.au
 
 
 """
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import sys
 import pathlib
 ## Importing especific functions used in this program 
@@ -24,38 +21,37 @@ from functions import *
 
 # Change the name of the file if you want to see separately
 
-resultsFile   = "resultsByVaringbatchSize.csv"
+resultsFile   = "resultsByVaringOptimezer.csv"
 
 ## You can define any array with possible values and run
-
-batchSizeToSimulate = [128]  
+valuesToSimulate = ['RMSprop', 'Adagrad', 'Adadelta', 'Adamax', 'Nadam']
 
 
 # Change the loop and the attribution, in this case on line 42 to check results
 
-for attempt in range(0,len(outputACTFNNSizeToSimulate)):
+for attempt in range(0,len(valuesToSimulate)):
     
-    print("\nSimulating with OUTPUT_ACTIVATIONS_FNN size " + str(outputACTFNNSizeToSimulate[attempt]) + "...\n")
+    print("\nSimulating with optmiser " + str(valuesToSimulate[attempt]) + "...\n")
   
     # Setting Hyperparameters
     noOfEpochs  = 9   # define number of epochs to execute
-    myBatchSze  = batchSizeToSimulate[attempt]  # size of each batch in interaction to get an epoch
-    myTestSize  = 0.2 # how much have to be split for testing
+    myBatchSze  = 128  # size of each batch in interaction to get an epoch
+    myTestSize  = 0.25 # how much have to be split for testing
     noOfFiles   = 5   # number of batch files to process
     myMinDelta  = 0.01 # minimum improvement rate for do not early stop
     myPatience  = 2   # how many epochs run with improvement lower than myMinDelta 
     MyRandomSt  = 42  # random state for shuffling the data
     myMetric    = "accuracy" # type of metrics used 
-    MyOptimizer = "adam"
+    MyOptimizer = valuesToSimulate[attempt]
     MyLoss      = "categorical_crossentropy"
     MyLearnRate = 0 # 0 value will keep default. Eg. adam => 0.001 
     noLayersCNN = 4
     noFiltersCNN= 32 # it will increase by plus 32 for each hidden layer
-    hiddenActCNN= 'tanh'  #'relu'
-    outputActCNN= 'softmax' #'relu'
+    hiddenActCNN= 'relu'  #
+    outputActCNN= 'relu'
     dropOutsCNN = []
     noLayersFNN = 2
-    noNeuronsFNN= 10 # output layer. It will multly for each hidden layer 
+    noNeuronsFNN= 5 # output layer. It will multly for each hidden layer  
     hiddenActFNN= 'relu'
     outputActFNN= 'softmax'
     dropOutsFNN = []
@@ -84,34 +80,5 @@ for attempt in range(0,len(outputACTFNNSizeToSimulate)):
         outputActFNN,
         dropOutsFNN 
         )
-
-     
-# reading the results and plot the comparison with accuracy
-resultsDf = pd.read_csv(path + resultsFile)
-
-resultsDf.columns = [
-"TRAINING_SIZE", "TEST_SIZE", "RANDOM_STATE", "N_EPOCHS", "N_LAYERS_CNN",
-"TOTAL_FILTERS_CNN", "HIDDEN_ACTIVATIONS_CNN", "OUTPUT_ACTIVATION_CNN",
-"LIST_OF_DROPOUTS_CNN", "N_LAYERS_FNN" , "TOTAL_NEURONS_FNN", 
-"HIDDEN_ACTIVATIONS_FNN" , "OUTPUT_ACTIVATIONS_FNN", "DROPOUTS_FNN",
-"BATCHSIZE", "MINDELTA" , "PATIENCE" , "METRIC" ,  "OPTMIZER",
-"LEARN_RATE", "TYPE_OF_LOSS" ,  "LOSS_VALUE" , "ACCURACY", "F1_SCORE" , 
-"PRECISION", "RECALL" , "F-BETA02", "F-BETA2"]
-
-resultsDf = resultsDf.astype({"OUTPUT_ACTIVATIONS_FNN": float})
-    
-# Define llenDf colours 
-jet = plt.get_cmap('jet')
-colors = iter(jet(np.linspace(0,1,len(resultsDf.index))))
-
-plt.title('Comparision Accuracy vs OUTPUT_ACTIVATIONS_FNN')    
-plt.xlabel("OUTPUT_ACTIVATIONS_FNN") 
-plt.ylabel("Accuracy" )
-
-for index, row in resultsDf.iterrows():
-   # Plotting values in different colours
-   plt.scatter(row["OUTPUT_ACTIVATIONS_FNN"],row["ACCURACY"], color= next(colors) )
-
-plt.show()
         
    
---
+++ source_code.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Anomalies Detection on images
+Date: 24/01/2021
+Author: Diego Bueno (ID: 23567850) / Isabelle Sypott (ID: 21963427 )
+e-mail: d.bueno.da.silva.10@student.scu.edu.au / i.sypott.10@student.scu.edu.au
+
+"""
+
+## importing the libraries required
+import numpy as np
+import sys
+import pathlib
+import random
+import tensorflow as tf # using Tensorflow 2.4
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split #used to split data into training and test segments
+from keras.callbacks import EarlyStopping, ModelCheckpoint 
+#from keras import backend as K
+#K.set_image_dim_ordering('tf') # Setting tensorflow-style ordering (NHWC).
+
+## Importing especific functions used in this program 
+path = str(pathlib.Path(__file__).resolve().parent) + "/"
+sys.path.append(path)
+from functions import *
+
+# initialising variables
+data        = [] # array with all list of images read from batch files
+X           = [] # array with images data including channels (RGB)
+y           = [] # array with labels (index of category of images)
+myModelFile = path + "ourmodel.h5" # file to save trained model
+
+# Setting Hyperparameters
+noOfEpochs  = 9   # define number of epochs to execute
+myBatchSze  = 128 # size of each batch in interaction to get an epoch
+myTestSize  = 0.25# how much have to be split for testing
+noOfFiles   = 5   # number of batch files to process
+myMinDelta  = 0.01# minimum improvement rate for do not early stop
+myPatience  = 2   # how many epochs run with improvement lower than myMinDelta 
+MyRandomSt  = 42  # random state for shuffling the data
+myMetric    = "accuracy" # type of metric used for training
+MyOptimizer = "adam"
+MyLoss      = "categorical_crossentropy"
+
+# Loading the data. "images/" folder must be in the same location of the script
+#
+# Structure of meta dictionary
+#
+## meta dictionary:
+    # {   
+    # num_cases_per_batch': 10000
+    #                      0           1            2       3        4       5        6        7         8         9
+    # label_names': [b'airplane', b'automobile', b'bird', b'cat', b'deer', b'dog', b'frog', b'horse', b'ship', b'truck']
+    # b'num_vis':    3072 ( 1024 R + 1024 G + 1024 B )   
+    # } 
+meta = read( path + 'images/batches.meta')
+
+#
+# Structure of data dictionary
+#
+## data: data_batch_N dictionary
+    # { 
+    # b'labels': b'training batch 5 of 5' => title of dictionary
+    # b'labels': [1, 8... n ] => array 1D of size 10,000 labels
+    # b'data': array([[255, 252, 253,..] , [127, 126, 127, ...], [...]] ) => array of size 10,000 x 3,072 ( 1024 R + 1024 G + 1024 B )
+    # b'filenames': [b'compact_car_s_001706.png', b'icebreaker_s_001689.png',...] => array of size 10,000 with files names
+    # }        
+for n in range(1,noOfFiles + 1,1):
+    data = np.append(data, read( path + 'images/data_batch_' + str(n)) )
+
+# Unifying all images pixels values in unique array X with 50,000 images
+for images in data: # data = { data_batch_1, data_batch_2, data_batch_3, data_batch_4, data_batch5   }
+    if len(X) == 0:
+        X = images[b'data'] 
+    else: # Need to concatenate instead append to create a unique array with 50,000 images
+        X = np.concatenate((X, images[b'data']), axis=0)
+    y = np.append(y, images[b'labels'] ) # y [ 0,1,2,3 ... 50,000] with 50,000 labels
+
+print("Shape of inputted data")        
+print("\nX shape with all images data: ", X.shape)
+print("y shape with all images labels", y.shape)
+
+# Selecting 40,000 instances for training set and 10,000 for test set
+[x_train, x_test, y_train, y_test] = train_test_split(X, y, test_size = myTestSize, random_state= MyRandomSt )
+
+print("\nShape of split data")
+print("\nx_train initial shape:  ", x_train.shape)
+print("x_test initial shape:  ", x_test.shape)
+print("y_train initial shape:  ", len(y_train))
+print("y_test initial shape:  ", len(y_test))
+print("\n")
+ 
+# Pre-processing to work with values between 0.0 and 1
+x_trainNormalised = x_train / 255.0
+x_testNormalised = x_test / 255.0
+
+## Changing the shape of inputted data
+nInstancesTrain  = x_trainNormalised.shape[0]
+nInstancesTest   = x_testNormalised.shape[0]
+nRowns           = 32 
+nColumns         = 32
+nChannels        = 3  # 3 channels denotes one red, green and blue (RGB image)
+
+#x_trainNormalised= x_trainNormalised.reshape(   40,000,          32,     32,       3     )
+x_trainNormalised = x_trainNormalised.reshape(nInstancesTrain, nRowns, nColumns, nChannels) 
+
+#x_testNormalised = x_testNormalised.reshape(  10,000,        32,      32,       3     )
+x_testNormalised = x_testNormalised.reshape(nInstancesTest, nRowns, nColumns, nChannels)
+
+# Changing the shape of OUTPUT layer, also changing the labels of train and test into categorical data
+# It creates hot vectors for the classes like: [0. 0. 0. 1. 0. 0. 0. 0. 0. 0.]
+y_trainCategorical = to_categorical(y_train)
+y_testCategorical = to_categorical(y_test)
+
+# Checking that train and test data categorical and normalised are the correct shape for NN
+print("\nShape of normalised data")
+print("\nx_train normalised shape: ", x_trainNormalised.shape)
+print("x_test normalised shape: ", x_testNormalised.shape)
+print("y_trainCategorical shape: ", y_trainCategorical.shape)
+print("y_testCategorical shape: ", y_testCategorical.shape)
+print("\n\n")
+
+
+## Loading previously trained model
+# If the model has been trained before, load it
+try:
+    model = tf.keras.models.load_model(myModelFile,
+            custom_objects={'f1_m': f1_m,
+                            'precision_m': precision_m, 
+                            'recall_m': recall_m, 
+                            'fbetaprecisionskewed': fbetaprecisionskewed, 
+                            'fbetarecallskewed': fbetarecallskewed})
+    print("Model file " + myModelFile + " sucessfully loaded!\n")
+    
+except OSError:
+    print("Previously saved model does not exist.\n" +
+          "Creating first file " + myModelFile + "\n")
+except:
+    print("Error trying to open the file " + myModelFile +
+          "\nCurrent file will be replaced!\n")
+ 
+    
+## Deploying the best configuration for Deep Neural Networking
+#  found by findingTheBestHyperparameters.py
+
+# designing the Convolutional Neural Network 
+model = tf.keras.models.Sequential() 
+                                              #32    #32        #3           
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), input_shape = (nRowns, nColumns, nChannels), activation='relu'))           
+model.add(tf.keras.layers.MaxPooling2D(pool_size = (2, 2))) #, strides=2)) testing if strides improve accuracy
+#model.add(tf.keras.layers.Dropout(0.8)) 
+
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')) 
+model.add(tf.keras.layers.MaxPool2D(pool_size = (2,2)))
+#model.add(tf.keras.layers.Dropout(0.5)) 
+
+model.add(tf.keras.layers.Conv2D(filters=96, kernel_size=(3,3), activation='relu', padding='same'))
+model.add(tf.keras.layers.MaxPool2D(pool_size = (2,2)))
+model.add(tf.keras.layers.Dropout(0.4))
+
+model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same'))
+model.add(tf.keras.layers.MaxPool2D(pool_size = (2,2)))
+#model.add(tf.keras.layers.Dropout(0.5))
+
+
+# designing by Fully Connect Neural Network
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation='relu'))    
+model.add(tf.keras.layers.Dense(128, activation='relu'))    
+model.add(tf.keras.layers.Dense(64, activation='relu'))    
+model.add(tf.keras.layers.Dense(32, activation='relu'))   
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    
+## compile DNN => not sparse_categorical_crossentropy because classes are exclusives!
+model.compile(optimizer=MyOptimizer, loss=MyLoss, metrics=[myMetric, f1_m, precision_m, recall_m, fbetaprecisionskewed, fbetarecallskewed])
+
+# Stopping early according to myMinDelta to avoid overfitting. Trained model saved at myModelFile
+myCallbacks = [EarlyStopping(monitor=myMetric, min_delta=myMinDelta , patience=myPatience, mode='auto'),
+             ModelCheckpoint(filepath=myModelFile, monitor=myMetric, save_best_only=True, verbose=1)]
+ 
+## Training the model according to the labels and chose hyperparameters
+model.fit(x_trainNormalised, y_trainCategorical, epochs=noOfEpochs, batch_size=myBatchSze,  verbose=1, callbacks = myCallbacks)
+
+## evaluating the accuracy using test data
+loss_val, acc_val, f1_score, precision, recall, fbetaprecisionskewed, fbetarecallskewed = model.evaluate(x_testNormalised, y_testCategorical)
+print('\n\nAccuracy: ', acc_val)
+print('Loss: ', loss_val)
+print('F1 Score: ', f1_score)
+print('Precision: ', precision)
+print('Recall: ', recall)
+print('F- Beta (0.2) Score: ', fbetaprecisionskewed)
+print('F- Beta (2) Score: ', fbetarecallskewed)
+
+# Predicting aleatory sample from 0 to 10,000 (test set has 10,000 instances)
+someSample = random.randint(0, (len(X)*myTestSize) - 1 ) 
+y_predicted = np.argmax(model.predict(x_testNormalised), axis=-1)
+
+print("y_predicted for test dataset index ",someSample," is ", meta[b'label_names'][y_predicted[someSample]])
+
+# Print the image
+printImage(x_test[someSample])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
